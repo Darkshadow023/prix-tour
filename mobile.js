d66db9657f80fd0bf174bc6b218929f64215e386
adityakapoor-brawl <77436849+adityakapoor-brawl@users.noreply.github.com>
2023-10-06 23:28:00 +0530

    Add files via upload

---
+++ mobile.js
@@ -0,0 +1,170 @@
+const doc = document;
+const menuOpen = doc.querySelector(".menu");
+const menuClose = doc.querySelector(".close");
+const overlay = doc.querySelector(".overlay");
+
+menuOpen.addEventListener("click", () => {
+  overlay.classList.add("overlay--active");
+});
+
+menuClose.addEventListener("click", () => {
+  overlay.classList.remove("overlay--active");
+});
+
+
+function test1(){
+  document.getElementById('seat1').style.backgroundColor="rgb(12, 210, 12)"
+  
+}
+function test2(){
+document.getElementById('seat2').style.backgroundColor="rgb(12, 210, 12)"
+
+}
+
+function test3(){
+  document.getElementById('seat3').style.backgroundColor="rgb(12, 210, 12)"
+  
+}
+
+function test4(){
+  document.getElementById('seat4').style.backgroundColor="rgb(12, 210, 12)"
+  
+}
+
+function test5(){
+  document.getElementById('seat5').style.backgroundColor="rgb(12, 210, 12)"
+  
+}
+
+function test6(){
+  document.getElementById('seat6').style.backgroundColor="rgb(12, 210, 12)"
+  
+}
+
+function test7(){
+  document.getElementById('seat7').style.backgroundColor="rgb(12, 210, 12)"
+  
+}
+
+function test8(){
+  document.getElementById('seat8').style.backgroundColor="rgb(12, 210, 12)"
+  
+}
+
+function test9(){
+  document.getElementById('seat9').style.backgroundColor="rgb(12, 210, 12)"
+  
+}
+
+function test10(){
+  document.getElementById('seat10').style.backgroundColor="rgb(12, 210, 12)"
+  
+}
+
+function test11(){
+  document.getElementById('seat11').style.backgroundColor="rgb(12, 210, 12)"
+  
+}
+
+function test12(){
+  document.getElementById('seat12').style.backgroundColor="rgb(12, 210, 12)"
+  
+}
+
+function test13(){
+  document.getElementById('seat13').style.backgroundColor="rgb(12, 210, 12)"
+  
+}
+
+function test14(){
+  document.getElementById('seat14').style.backgroundColor="rgb(12, 210, 12)"
+  
+}
+
+function test15(){
+  document.getElementById('seat15').style.backgroundColor="rgb(12, 210, 12)"
+  
+}
+
+function test16(){
+  document.getElementById('seat16').style.backgroundColor="rgb(12, 210, 12)"
+  
+}
+
+function test17(){
+  document.getElementById('seat17').style.backgroundColor="rgb(12, 210, 12)"
+  
+}
+
+function test18(){
+  document.getElementById('seat18').style.backgroundColor="rgb(12, 210, 12)"
+  
+}
+
+function test19(){
+  document.getElementById('seat19').style.backgroundColor="rgb(12, 210, 12)"
+  
+}
+
+function test20(){
+  document.getElementById('seat20').style.backgroundColor="rgb(12, 210, 12)"
+  
+}
+
+function test21(){
+  document.getElementById('seat21').style.backgroundColor="rgb(12, 210, 12)"
+  
+}
+
+function test22(){
+  document.getElementById('seat22').style.backgroundColor="rgb(12, 210, 12)"
+  
+}
+
+function test23(){
+  document.getElementById('seat23').style.backgroundColor="rgb(12, 210, 12)"
+  
+}
+
+function test24(){
+  document.getElementById('seat24').style.backgroundColor="rgb(12, 210, 12)"
+  
+}
+
+function test25(){
+  document.getElementById('seat25').style.backgroundColor="rgb(12, 210, 12)"
+  
+}
+
+function test26(){
+  document.getElementById('seat26').style.backgroundColor="rgb(12, 210, 12)"
+  
+}
+
+function test27(){
+  document.getElementById('seat27').style.backgroundColor="rgb(12, 210, 12)"
+  
+}
+
+function test28(){
+  document.getElementById('seat28').style.backgroundColor="rgb(12, 210, 12)"
+  
+}
+
+function test29(){
+  document.getElementById('seat29').style.backgroundColor="rgb(12, 210, 12)"
+  
+}
+
+function test30(){
+  document.getElementById('seat30').style.backgroundColor="rgb(12, 210, 12)"
+  
+}
+
+function confirmed(){
+  window.open("redirect.html", "_self");
+}
+
+function seats(){
+  window.open("seats.html", "_self");
+}
